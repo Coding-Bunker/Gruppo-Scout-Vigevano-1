fa247d82dd83fc8b9ee360e777a05f73cdffbf37
ITMASTER\g.monteleone <g.monteleone@almaviva.it>
2021-02-11 22:38:13 +0100

    fix footer mobile problems

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
     <script src="<?php echo get_stylesheet_directory_uri(); ?>/assets/js/bootstrap.min.js" ></script>
     <footer>
         <div class="container">
-            <div class=" row justify-content-start">
+            <div class=" row justify-content-start text-center text-xl-left text-lg-left text-md-center text-sm-center">
                 <div class="mt-2 mb-2 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <span>Seguici sui social</span>
                     <br>
@@ -30,18 +30,18 @@
                 </div>
                 <div class="mt-2 mb-2 col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
                     <span>Naviga</span>
-                    <div class="row">
-                        <div class="col-6">
-                            <a href="">home</a>
+                    <div class="row justify-content-sm-center justify-content-center">
+                        <div class="col-3 col-sm-3 col-md-6 col-lg-6 col-xl-6">
+                            <a href="">Home</a>
                         </div>
-                        <div class="col-6"><a href="">braci</a></div>
+                        <div class="col-3 col-sm-3 col-md-6 col-lg-6 col-xl-6"><a href="">Braci</a></div>
+                    </div>
+                    <div class="row justify-content-sm-center justify-content-center">
+                        <div class="col-3 col-sm-3 col-md-6 col-lg-6 col-xl-6"><a href="">Chi siamo</a></div>
+                        <div class="col-3 col-sm-3 col-md-6 col-lg-6 col-xl-6"><a href="">Contatti</a></div>
                     </div>
                     <div class="row">
-                        <div class="col-6"><a href="">chi siamo</a></div>
-                        <div class="col-6"><a href="">contatti</a></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-6"><a href="">Branche</a></div>
+                        <div class="col-3 offset-3 offset-sm-3 offset-md-0 offset-lg-0 offset-xl-0 col-sm-3 col-md-6 col-lg-6 col-xl-6"><a href="">Branche</a></div>
                     </div>
                 </div>
             </div>

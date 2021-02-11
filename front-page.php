fb57cef4ad62d5ac691cf115018121069bd238df
diskxo <nicola.preda05@gmail.com>
2021-02-12 00:40:01 +0100

    Added "where-we-are" section
    🎨STYLE:
    Added "where-we-are" section.
    Decreased the size of the content text.
    Added footer margin-top("5vh")

--- front-page.php
+++ front-page.php
@@ -10,9 +10,9 @@ include('template-parts/header-home.php')
             </div>
             <div class="col">
                 <h2>Chi siamo</h2>
-                <h3>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                <h4>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
-                </h3>
+                </h4>
                 <button type="button" class="btn btn-success">ENTRA NEL GRUPPO</button>
             </div>
         </div>
@@ -68,5 +68,22 @@ include('template-parts/header-home.php')
     </div>
 
 
+</section>
+
+<section class="where-we-are">
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col">
+                <h2>Chi siamo</h2>
+                <h4>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                </h4>
+                <button type="button" class="btn btn-success">ENTRA NEL GRUPPO</button>
+            </div>
+            <div class="col-md-4 px-0">
+                <img class="img-fluid" style="height: 75%;"src="https://www.google.com/maps/d/u/0/thumbnail?mid=1ksQvIlnDYIH3zAmUj3R-E0Goei4&hl=it" alt="Immagine dove siamo" />
+            </div>
+        </div>
+    </div>
 </section>
 <?php get_footer(); ?>

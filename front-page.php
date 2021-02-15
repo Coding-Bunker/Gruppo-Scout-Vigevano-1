c6c34fdc54c9d1a543f4f2f86fcaa46462a9ebb0
diskxo_ <nicola.preda05@gmail.com>
2021-02-15 23:57:21 +0100

    🎉FEATURE: Added "Specialità... Fotografo!" section

--- front-page.php
+++ front-page.php
@@ -74,7 +74,7 @@ include('template-parts/header-home.php')
     <div class="container">
         <div class="row mt-5">
             <div class="col">
-                <h2>Chi siamo</h2>
+                <h2>Dove siamo</h2>
                 <h4>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
                     industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
                 </h4>
@@ -86,6 +86,52 @@ include('template-parts/header-home.php')
         </div>
     </div>
 </section>
+
+<div class="container gallery-container">
+
+    <h1>Specialità…
+        Fotografo!</h1>
+    
+    <div class="tz-gallery">
+
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="assets/images/scout6.png">
+                    <img class="gallery rounded" src="assets/images/scout6.png" alt="Scout Image 1">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="assets/images/scout5.png">
+                    <img class="gallery rounded" src="assets/images/scout5.png" alt="Scout Image 2">
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <a class="lightbox" href="assets/images/scout4.png">
+                    <img class="gallery rounded" src="assets/images/scout4.png" alt="Scout Image 3">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox gallery" href="assets/images/scout3.png">
+                    <img class="gallery rounded" src="assets/images/scout3.png" alt="Scout Image 4">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="assets/images/scout2.png">
+                    <img class="gallery rounded" src="assets/images/scout2.png" alt="Scout Image 5">
+                </a>
+            </div>
+            <div class="col-sm-6 col-md-4">
+                <a class="lightbox" href="assets/images/scout1.png">
+                    <img class="gallery rounded" src="assets/images/scout1.png" alt="Scout Image 5">
+                </a>
+            </div>
+            
+        </div>
+
+    </div>
+
+</div>
+
 <section class="join-group bg-blue mt-5">
     <h2 class="text-center pt-5 mb-5">Entra nel gruppo</h2>
     <div class="container">

6cd14aa2c78b172a300391f13877a47265716a99
ITMASTER\g.monteleone <g.monteleone@almaviva.it>
2021-03-06 17:21:59 +0100

    aggiunta pagina 404

--- 404.php
+++ 404.php
@@ -1,3 +1,26 @@
-<?php
+<?php 
 
+include('header.php')
+?>
+<section class="error-page">
+    <div class="container">
+        <div class="row justify-content-center">
+            <img alt="ERRORE" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/404_img.png"/>
+        </div>
+        <div class="row justify-content-center">
+            <span class="text-error">ERRORE 404</span>
+        </div>
+        <div class="row justify-content-center">
+            <span class="text-title">Ti sei perso nel bosco?</span>
+        </div>
+        <div class="row justify-content-center">
+            <span class="text-title">Torna al sentiero</span>
+        </div>
+        <div class="row justify-content-center">
+            <a href="/" class="btn btn-lg btn-success bg-green">Homepage</a>
+        </div>
+    </div>
+</section>
+<?php
+    get_footer();
 ?>
--- template-parts/header-home.php
+++ template-parts/header-home.php
@@ -36,9 +36,51 @@
                 <div class="announcement text-center">
                     <span>TESTO DA AGGIUNGERE IN BE</span>
                 </div>
-                <?php 
-                    include('navbar.php')
-                ?>
+                <nav class="container nav navbar navbar-expand-lg navbar-dark">
+                    <a href="/" class="navbar-brand">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/logo_bianco.svg" width="75" height="75" alt="logo">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+                        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Chi siamo <span class="sr-only">(current)</span></a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Braci<span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Blog <span class="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contatti <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link btn btn-outline-light" href="#">Entra nel gruppo</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
                 <div class=" mb-5 mt-5 container">
                     <h1 class="header-title">Benvenuti sul sito del Gruppo Scout Vigevano 1</h1>
                 </div>
